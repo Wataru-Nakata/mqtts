54f3bffbf6a4692e8abaa4296b2586330f393cdb
Wataru Nakata <wataru9871@gmail.com>
2023-08-01 20:52:00 +0900

    changed matmul precision to medium

--- examples/train_quantizer.py
+++ examples/train_quantizer.py
@@ -10,6 +10,7 @@ from mqtts_lightning.model.quantizer_lightning_module import QuantizerLightningM
 @hydra.main(version_base="1.3", config_name="config", config_path="./configs")
 def main(cfg: DictConfig):
     seed_everything(1234)
+    torch.set_float32_matmul_precision("medium")
     lightning_module = QuantizerLightningModule(cfg)
     datamodule = MQTTSQuantizerDataModule(cfg)
     loggers = hydra.utils.instantiate(cfg.train.quantizer.loggers)
